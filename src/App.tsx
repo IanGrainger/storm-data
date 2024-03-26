b410c5573dc89651e251cb8920ad5083df2a4a01
Ian Grainger <ian.grainger@gmail.com>
2024-03-26 14:39:53 +0000

    install solid-select and add multi selection example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Select } from '@thisbeyond/solid-select';
 import type { Component } from 'solid-js';
 import BuildingList from './components/buildingList';
 
 const App: Component = () => {
-  return <BuildingList />;
+  return (
+    <div>
+      <Select
+        multiple
+        options={['apple', 'banana', 'pear', 'pineapple', 'kiwi']}
+      />
+      <BuildingList />
+    </div>
+  );
 };
 
 export default App;
