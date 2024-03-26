1698970f881957b0e9c943fa7dd67bde477e8b55
Ian Grainger <ian.grainger@gmail.com>
2024-03-26 15:15:07 +0000

    attempt to solve static file paths for GH pages with this answer: https://github.com/vitejs/vite/discussions/12242#discussioncomment-7286359

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import { render } from 'solid-js/web';
 
 import App from './App';
 
+const base = document.createElement('base');
+base.href = import.meta.env.BASE_URL;
+document.head.insertBefore(base, document.head.firstChild);
+
 const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?'
   );
 }
 
