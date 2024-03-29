88b00f980ac57e3b2861cb9cc24b5a0aba252247
Ian Grainger <ian.grainger@gmail.com>
2024-03-29 06:52:49 +0000

    add a resources selector - but thats not quite what i need

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { BuildingList } from './components/BuildingList';
 import { BuildingSelect } from './components/BuildingSelect';
 import { Options } from './components/Options';
 import { OptionsProvider } from './providers/OptionsProvider';
+import { ResourcesSelect } from './components/ResourcesSelect';
 
 export const App: Component = () => {
   const selectedBuildingsSignal = createSignal([] as string[]);
+  const selectedResourcesSignal = createSignal([] as string[]);
 
   return (
     <div>
       <OptionsProvider>
         <Options />
+        <ResourcesSelect signal={selectedResourcesSignal} />
         <BuildingSelect signal={selectedBuildingsSignal} />
         <BuildingList showNamesAccr={selectedBuildingsSignal[0]} />
       </OptionsProvider>
---
+++ src/components/ResourcesSelect.tsx
@@ -0,0 +1,29 @@
+import { createOptions, Select } from '@thisbeyond/solid-select';
+import { Component, Signal } from 'solid-js';
+import resources from 'src/data/ResourcesList_Grouped.json';
+
+export const ResourcesSelect: Component<{ signal: Signal<string[]> }> = (
+  props
+) => {
+  const goods = resources.Goods;
+  const resourceNames = goods.map((good) => good.Name);
+  const [selectedValues, setSelectedValues] = props.signal;
+
+  const onChange = (selected: string[]) => {
+    setSelectedValues(selected);
+  };
+
+  const resourcesOptions = createOptions(resourceNames, {
+    disable: (value: string) => selectedValues().includes(value),
+  });
+
+  return (
+    <Select
+      placeholder="Select resources..."
+      class="resourcesList"
+      multiple
+      onChange={onChange}
+      {...resourcesOptions}
+    />
+  );
+};
